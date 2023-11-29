47fef2b10891868e37d35109b924968e5bfeeb70
Norman Huth <norman@huth.it>
2023-11-29 11:37:56 +0100

    feat: add welcome page

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [
+    laravel({
+      input: ['resources/scss/app.scss'],
+      refresh: true
+    })
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          return assetInfo.name
+        }
+      }
+    }
+  }
+})
